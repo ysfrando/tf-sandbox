92f8a1f610452600df98dc003212b67ef739c90b
ysf.sec <yusufmunir.sec@gmail.com>
2024-11-26 14:04:39 +0500

    modify file: main.tf

--- projects/secure-defaults/main.tf
+++ projects/secure-defaults/main.tf
@@ -1,6 +1,6 @@
 module "secure_module" {
   source                  = "./modules/secure_module"
-  vpc_id                  = "vpc-0931b62e3647f32eb"
+  vpc_id                  = "your-vpc-id"
   bucket_name             = "my-project-bucket-ysfrando-123434252"
   allowed_ports           = [22, 80, 443]
   allowed_cidrs           = ["192.168.1.0/24"]
